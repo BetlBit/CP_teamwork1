e1f4f985b9d3e07d88c694442026cd282ea1a692
BetlBit <inthebetmen@mail.ru>
2022-05-03 08:29:28 +0300

    footer

--- myapp/src/Footer.js
+++ myapp/src/Footer.js
@@ -1,37 +1,46 @@
 import React from "react";
+import ico1 from "./images/facebook.png";
+import ico2 from "./images/twitter.png";
+import ico3 from "./images/telegram.png";
+import ico4 from "./images/youtube.png";
 import "./style.css";
 
 
 export const Footer = () => {
-    return (
-        <div className="footer">
-            <div className="block-one">
-                <div className="logo-footer">Axie</div>
-                <p className="info">IC New Town Street, Newstate U94</p>
-                <p className="info">(+88)017857414</p>
-                <p className="info">supporterAxie@gmail.com</p>
-            </div>
-            <div className="block-two">
-                <div className="other-info">
-                    <h2 className="other-pages">Other Pages</h2>
-                    <p className="pages">About Us</p>
-                    <p className="pages">Privacy</p>
-                    <p className="pages">Contact Us</p>
-                </div>
-                <div className="privacy-teams">
-                    <h2 className="privacy-info">Privacy & Teams</h2>
-                    <p className="pages">Software principles</p>
-                    <p className="pages">Application securiy</p>
-                    <p className="pages">White paper</p>
-                </div>
-            </div>
-            <div className="block-three">
-                <h2 className="follow">Follow Us</h2>
-                <div className="messangers"></div>
-                <div className="messangers"></div>
-                <div className="messangers"></div>
-                <div className="messangers"></div>
-            </div>
-        </div>
-    )
+	return (
+		<div className="footer">
+			<footer className='footer'>
+				<div>
+					<h1 className="header-title"><span className="span-UP">Axie</span></h1>
+					<p id='address'>г. Лаишево, г. Лаишево, ул. Советская, дом 4 <br />8 (960) 074-94-74<br />support@gmail.com</p>
+				</div>
+
+				<div>
+					<p className='headingC'><b>Links</b></p>
+					<ul>
+						<li><a href="/home">Home</a></li>
+						<li><a href="/about">About</a></li>
+						<li><a href="">Contact us</a></li>
+					</ul>
+				</div>
+
+				<div>
+					<p className='headingC'><b>Info</b></p>
+					<ul>
+						<li><a href="">Partners</a></li>
+						<li><a href="">Licenses</a></li>
+						<li><a href="">Legal information</a></li>
+					</ul>
+				</div>
+
+				<div>
+					<p id='c4' className='headingC'><b>Follow us</b></p>
+					<img className='followUS' src={ico1} alt='Facebook' />
+					<img className='followUS' src={ico2} alt='Twitter' />
+					<img className='followUS' src={ico3} alt='Telegram' />
+					<img className='followUS' src={ico4} alt='YouTube' />
+				</div>
+			</footer>
+		</div>
+	)
 }
