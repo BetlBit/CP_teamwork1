31d14f65c0630925594c02de0069001429c0e3b8
AndYuz27 <yuzlov2003@gmail.com>
2022-05-12 19:02:44 +0300

    aaaaaa

--- myapp/src/Navbar.js
+++ myapp/src/Navbar.js
@@ -1,18 +1,22 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
 import shield from './images/shield.png'
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
 
 
 export const Navbar = () => {
+	const theme = useContext(ThemeContext)
     return (
-        <header className="header">
+        <header className="header" style={{background: theme.background, color: theme.color, outline: theme.outline}}>
         <nav className="nav">
             <img src={shield} className="logo"></img>
             <h1 className="header-title">Axie</h1>
             <a href="/home" className="links">Home</a>
             <a href="/product" className="links">Product</a>
             <a href="/about" className="links">About</a>
+
         </nav>
+
         <button className="sign-in">Log In</button>
       </header>
     )
--- myapp/src/Product.js
+++ myapp/src/Product.js
@@ -1,15 +1,18 @@
 import React from "react";
-import "./style.css";
-import correct from './images/correct.png'
-import remove from './images/remove.png'
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+
 
 
 export const Product = () => {
+    const theme = useContext(ThemeContext)
+
     return (
-        <div className="select-plan">
+        <div className="select-plan"  style={{background: theme.background, color: theme.color, outline: theme.outline}}>
             <h1 className="Buy-text">Buy Your Plan</h1>
-            <div className="select-box">
-                <div className="block-buy">
+            <div className="select-box" style={{color: theme.bl}}>
+
+                <div className="block-buy" >
                     <h2 className="option">Basic Plan</h2>
                     <p className="Price">$15<span>/per month</span></p>
                     <div className="describe">
